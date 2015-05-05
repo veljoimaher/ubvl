300d76d00b38092228930d24957ad106c87eb425
Velimir Iveljic <velimir.iveljic@agoracon.at>
2015-05-05 23:28:10 +0200

    .

--- node.c
+++ node.c
@@ -1,51 +1,168 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "node.h"
 
-
-struct list *insert_elem (int type, struct list *l, char *name)
+/* return pointer on newly created list
+ */
+struct list *list_create (void)
 {
+	struct list *new = (struct list *) malloc ( sizeof (struct list));
 
-        struct list *new;
-        struct list *head;
-        new = (struct list *) malloc (sizeof (struct list));
-        head = (struct list *) malloc (sizeof (struct list));
-
-        if (l == NULL)
-        {
-                printf ("passed list is empty: creating new list\n");
-                l = (struct list *) malloc (sizeof (struct list));
-                l->next = NULL;
-        }
-        else
-                printf ("passed non list is empty\n");
-
-        head = new;
-        while (l != NULL)
-        {
-                new->name = l->name;
-                new->type = l->type;
-                new->next = l->next;
-
-                new = new->next;
-                l = l->next;
-        }
-
-        printf ("inserting new element\n");
-        new->name = name;
-        new->type = type;
-        new->next = NULL;
-        new->name = name;
-
-        return head;
+	new->head.prev = NULL;
+	new->head.next = &new->tail;
+	new->tail.prev = &new->head;
+	new->tail.next = NULL;
+	return new;
 }
 
-struct list *merge_elems (struct list *l1, struct list *l2)
+void * list_insert (struct node *before, struct node *new)
 {
+	if (before == NULL || new == NULL)
+	{
+		fprintf (stderr, "list_insert() - provided node does not exist\n");
+		return NULL;
+	}
 
-        return NULL;
+	new->prev = before->prev;
+	new->next = before;
+	before->prev->next = new;
+	before->prev = new;
+	return new;
 }
 
+struct node * list_head (struct list *list)
+{
+	if (list == NULL)
+		return NULL;
+	return &list->head;
+}
+
+struct node * list_end (struct list *list)
+{
+	if (list == NULL)
+		return NULL;
+
+	return &list->tail;
+}
+
+struct node * list_begin (struct list *list)
+{
+	if (list == NULL)
+		return NULL;
+	return list->head.next;
+}
+
+struct node * list_back (struct list *list)
+{
+	if (list == NULL)
+		return NULL;
+
+	return list->tail.prev;
+}
+
+struct node * list_next (struct node *node)
+{
+	if (node == NULL)
+		return NULL;
+
+	return node->next;
+}
+
+struct list * list_push_back (struct list *list, struct node *node)
+{
+	list_insert (list_end (list), node);
+	return list;
+}
+
+
+
+/*
+ * FROM HERE ON FUNCTIONS FOR UEBERSETZERBAU
+ */
+
+
+/* quick and dirty but VERY expensive */
+struct list * list_merge_to_new (struct list *f, struct list *s)
+{
+	struct node *tmp_n;
+        struct node *fn = list_head (f);
+	struct node *sn = list_head (s);
+	struct list *new = list_create ();
+	struct list *new_p = new;
+
+        while ( (fn = list_next (fn)) != list_end (f))
+        {
+		tmp_n = (struct node *) malloc ( sizeof (struct node) );
+		tmp_n->name = fn->name;
+		tmp_n->val = fn->val;
+		tmp_n->type = fn->type;
+		list_push_back (new, tmp_n);
+        }
+
+	while ( (sn = list_next (sn)) != list_end (s))
+        {
+		tmp_n = (struct node *) malloc ( sizeof (struct node) );
+		tmp_n->name = sn->name;
+		tmp_n->val = sn->val;
+		tmp_n->type = sn->type;
+
+		if ( list_find (new_p, sn) != NULL) 
+			return NULL;
+
+		list_push_back (new, tmp_n);
+        }
+
+
+	return new;
+}
+
+struct node * list_find (struct list *list, struct node *node)
+{
+	struct node *n = list_head (list);
+	struct node *np = node;
+
+	if (np->name == NULL)
+		return NULL;
+
+	while ( (n = list_next (n)) != list_end (list))
+	{
+		if (n->name != NULL)
+		{
+			if (strncmp (n->name, np->name, strlen (np->name)) == 0)
+				return n;
+		}
+	}
+	return NULL;
+}
+
+struct list * insert_elem (int type, struct list *list, char *name)
+{
+	struct node * node = (struct node *) malloc ( sizeof (struct node) );
+	struct list * new_list = list_create ();
+	struct list * merged;
+
+	node->type = type;
+	node->name = name;
+	
+	list_push_back (new_list, node);
+	merged = list_merge_to_new (list, new_list);
+	if (merged == NULL)
+		exit (3);
+	return merged;
+}
+
+void list_dump (struct list *list)
+{
+	int cnt = 0;
+	struct node *n;
+	n = list_head (list);
+	while ( (n = list_next (n)) != list_end (list))
+	{
+		fprintf (stdout, "%d\t type: %d, name: '%s', val: %d\n", cnt, n->type, n->name, n->val);
+		cnt++;
+	}
+}
 void isPresent (struct list *l, char *name)
 {
 
--- node.h
+++ node.h
@@ -5,17 +5,35 @@
 #define FUNCTION    1
 #define VARIABLE    2
 
-struct list {
-
+struct node 
+{
+	struct node *next;
+	struct node *prev;
+	char *name;
         int type;
-        struct list *next; 
-        /* user defined data fields follow here */
-        char* name;
         int val;
 };
 
-struct list *insert_elem (int type, struct list *l, char *name);
-struct list *merge_elems (struct list *l1, struct list *l2);
+struct list
+{
+	struct node head;
+	struct node tail;
+};
+
+struct list * list_create (void);
+
+void * list_insert (struct node *before, struct node *new);
+struct node * list_head (struct list *list);
+struct node * list_begin (struct list *list);
+struct node * list_end (struct list *list);
+struct node * list_next (struct node *node);
+struct node * list_back (struct list *list);
+struct list * list_push_back (struct list *list, struct node *node);
+void list_dump (struct list *l);
+struct node * list_find (struct list *list, struct node *node);
+struct list * list_merge_to_new (struct list *first, struct list *second);
+struct list * insert_elem (int type, struct list *list, char *name);
+
 void isPresent (struct list *l, char *name);
 
 #endif
