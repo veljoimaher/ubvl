7f3e2f5a62da66431d94e16189ddc4192cf385f1
Emir Mahmutbegovic <e0326396@student.tuwien.ac.at>
2015-05-27 10:36:43 +0200

    register allocation changes

--- reg.c
+++ reg.c
@@ -8,16 +8,49 @@ char *temp_regs[] = { "r11", "rbx", "rax" };
 int args_regs_used[6] = {0, 0, 0, 0, 0, 0};
 int temp_regs_used[3] = {0, 0, 0};
 
+/* struct reg all_regs [9]; */
+struct reg all_regs[NR_REGS] =
+{
+	{"rdi", 0, PARAM},
+	{"rsi", 0, TEMP},
+	{"rdx", 0, TEMP},
+	{"rcx", 0, TEMP},
+	{"r8", 0, TEMP},
+	{"r9", 0, TEMP},
+	{"r11", 0, TEMP},
+	{"rbx", 0, TEMP},
+	{"rax", 0, TEMP}
+};
+
+void reg_init (struct list *l)
+{
+	int i;
+	int nr_params = 0;
+	struct node *n = list_head (l);
+	while ( (n = list_next (n)) != list_end (l))
+	{
+		if (n->type == PARAMETER)
+			nr_params++;
+	}
+
+	for (i=0; i<nr_params; i++)
+	{
+		all_regs[i].type = PARAM;
+		all_regs[i].used = 1;
+	}
+}
+
 char * newreg()
 {
 	int i;
-	for (i = 0; i<3; i++)
+	for (i = 0; i<NR_REGS; i++)
 	{
-		if (temp_regs_used[i] == 0)
+		if ( all_regs[i].used == 0 )
 		{
-			temp_regs_used[i] = 1;
-			return temp_regs[i];
+			all_regs[i].used = 1;
+			return all_regs[i].name;
 		}
+
 	}
 	printf ("no new regs\n");
 	return "unavail";
@@ -26,10 +59,22 @@ char * newreg()
 void freereg(char * reg)
 {
 	int i;
-	for (i = 0; i<3; i++)
+	for (i = 0; i<NR_REGS; i++)
 	{
-		if (strcmp (reg, temp_regs[i]) == 0)
-			temp_regs_used[i] = 0;
+		if (strcmp (reg, all_regs[i].name) == 0)
+			all_regs[i].used = 0;
 	}
 }
 
+void freeallreg ()
+{
+	int i;
+	all_regs[0].used = 0;
+	all_regs[0].type = PARAM;
+
+	for (i=1; i<NR_REGS; i++)
+	{
+		all_regs[i].type = TEMP;
+		all_regs[i].used = 0;
+	}
+}
--- reg.h
+++ reg.h
@@ -1,8 +1,24 @@
 #ifndef __REG_H__
 #define __REG_H__
 
+#include "node.h"
+
+#define NR_REGS 9
+
+#define PARAM 0
+#define TEMP  1
+
+struct reg
+{
+        char *name;
+        int used;
+        int type;
+};
+
+void reg_init (struct list *l);
 char * newreg();
 void freereg(char * reg);
+void freeallreg ();
 
 #endif
 
